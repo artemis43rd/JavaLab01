5192c142d92977d72043efe8648c142409bd4de2
Artyom <shield2003@mail.ru>
2023-11-16 23:24:30 +0300

    Add Test2

--- src/test/java/ru/spbstu/telematics/java/AppTest.java
+++ src/test/java/ru/spbstu/telematics/java/AppTest.java
@@ -3,8 +3,8 @@ package ru.spbstu.telematics.java;
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
-public class AppTest {
-
+public class AppTest
+{
     @Test
     public void testSOE()
     {
@@ -15,11 +15,27 @@ public class AppTest {
         double[][] coef = {{2, 3, -1}, {4, -1, 2}, {-1, 2, -3}};
         double[] equally = {1, -2, 3};
 
-        double[] exSolution = {0, 0, -1};
+        double[] expectedSolution = {0, 0, -1};
 
         double[] solution = App.SOE(coef, equally);
 
-        // Проверка корректности решения с определенной точностью
-        assertArrayEquals(exSolution, solution, 1e-10);
+        assertArrayEquals(expectedSolution, solution, 1e-10);
+    }
+
+    @Test
+    public void testSolveAnotherLinearSystem()
+    {
+        // Другая система линейных уравнений:
+        // x + 2y + 3z = 6
+        // 2x + y + z = 4
+        // 3x + 3y + 4z = 11
+        double[][] coef = {{1, 2, 3}, {2, 1, 1}, {3, 3, 4.5}};
+        double[] equally = {6, 4, 11};
+
+        double[] expectedSolution = {1.333333333333333, -0.666666666666667, 2};
+
+        double[] solution = App.SOE(coef, equally);
+
+        assertArrayEquals(expectedSolution, solution, 1e-10);
     }
 }
